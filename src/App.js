8fda953a8ec24554d1039860c67c8a5793482cf4
Michał Grochalski <michal.grochalski@gmail.com>
2023-01-06 12:45:46 +0100

    Init project

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      How did we do?
+
+      Please let us know how we did with your support request. All feedback is appreciated
+      to help us improve our offering!
+
+      1 2 3 4 5
+
+      Submit
+
+
+      You selected  out of 5
+
+      Thank you!
+
+      We appreciate you taking the time to give a rating. If you ever need more support,
+      don’t hesitate to get in touch!
+
+
     </div>
   );
 }
